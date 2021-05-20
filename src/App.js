fb47f5f5b49220031d90aa08ef423ca890e9aac1
Viktoria-Iudina <jajudinaja@gmail.com>
2021-05-20 16:28:48 +0200

    Added search engine and button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import sticker from "./sticker.png";
+import books from "./books.png";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header>
-        <img src={sticker} alt="sticker" className="img-fluid" />
+      <header>  
+        <img src={books} alt="books" className="img-fluid pt-5" />
+        <div className="search">
+          <input className="form-control form-control-lg shadow" type="text" placeholder="Search a word"></input>
+          <a href="#" className="btn btn-light shadow">🔎</a>
+        </div>
       </header>
-      Hello
     </div>
   );
 }
