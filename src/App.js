ef8c56066c7a98487da6a5a88ef36b61781792b9
Viktoria-Iudina <jajudinaja@gmail.com>
2021-05-20 18:25:29 +0200

    Added an event listener to the search engine

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import books from "./books.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
+  function search() {
+    alert("searching");
+  }
   return (
     <div className="App">
-      <header>  
+      <div className="container">  
+        <header>
         <img src={books} alt="books" className="img-fluid pt-5" />
-        <div className="search">
-          <input className="form-control form-control-lg shadow" type="text" placeholder="Search a word"></input>
-          <button className="btn btn-light shadow"> 🔎 </button>
-        </div>
-      </header>
+          <div className="search">
+            <form onSubmit={search}>
+              <input className="form-control form-control-lg shadow" type="search" autoFocus={true} placeholder="Search a word"></input>
+            </form>
+            <button className="btn btn-light shadow"> 🔎 </button>
+          </div>
+        </header>
+
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center pt-5 pb-5">
+          <small>Coded by Viktoriia Iudina</small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+    return (
+    <div className="Dictionary">
+    </div>   
+    );
+}
