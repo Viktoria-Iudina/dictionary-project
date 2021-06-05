355c3a3eb8f569876af8779cc9f4a059bcd33388
Viktoria-Iudina <jajudinaja@gmail.com>
2021-06-05 11:12:59 +0200

    Integration with Pexels' API to display images for a searched word

--- src/App.js
+++ src/App.js
@@ -3,21 +3,30 @@ import books from "./books.png";
 import './App.css';
 import axios from "axios";
 import Results from './Results';
+import Photos from "./Photos";
 
 export default function App() {
   let [keyword, setKeyword] = useState("hello"); 
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey = "563492ad6f91700001000001fce9161f79ce404db68e11c647468a5c";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { "Authorization" : `Bearer ${pexelsApiKey}`};
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -50,6 +59,7 @@ export default function App() {
   
           <main>
             <Results results={results} />
+            <Photos photos={photos} />
           </main>
           <footer className="text-center pt-5 pb-5">
             <small>Coded by <a href="https://github.com/Viktoria-Iudina/dictionary-project">Viktoriia Iudina</a></small>
